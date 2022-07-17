24316ce29a80d2cc868bec76ca30eed0994b5d43
highlander <lxrbckl@gmail.com>
2022-07-16 20:35:11 -0500

    introduction of true backend of project fenaverat

--- main.py
+++ main.py
@@ -2,19 +2,29 @@
 
 
 # import <
-from backend.resource import getData # <
-from backend.resource import application
-from frontend.layout.template import templateFunction
+
 
 # >
+
+
+# global <
+githubToken = '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'
+discordToken = 'OTk4MDI3ODExNjM1ODU5NDk3.GvDOHI.GvbzpImhcZ1x7K-NLyJmnDTO4XdIuhd4w4m3HY'
+
+# >
+
+
+# TODO
+# - add/remove users to settings
+# - add/remove languages
+# - add/remove packages
+# - hide/show projects
+# - auto fetch data
 
 
 # main <
 if (__name__ == '__main__'):
 
-    getData() # <
-
-    # application.layout = templateFunction()
-    # application.run_server(debug = True)
+    pass
 
 # >
