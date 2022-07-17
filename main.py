033df7893fbce9fce4af184d7f6037219c3ee06c
highlander <lxrbckl@gmail.com>
2022-07-17 01:41:11 -0500

    introduction of true backend of project fenaverat

--- main.py
+++ main.py
@@ -2,24 +2,59 @@
 
 
 # import <
-
+from os import path
+from github import Github
+from discord import Intents
+from discord.ext import commands
+from lxRbckl import jsonLoad, jsonDump, githubSet
 
 # >
 
 
 # global <
+gPath = path.realpath(__file__).split('/')
+gDirectory = '/'.join(gPath[:(len(gPath) - 1)])
 githubToken = ''
+fenaverat = commands.Bot(command_prefix = '', intents = Intents.all())
 discordToken = ''
 
 # >
 
 
-# TODO
-# - add/remove users to settings
-# - add/remove languages
-# - add/remove packages
-# - hide/show projects
-# - auto fetch data
+async def setFunction(ctx, pKey: str, pAction: str, pValue: str, pData: dict):
+    '''  '''
+
+    pass
+
+
+async def getFunction(ctx, pKey: str, pAction: str, pValue: str, pData: dict):
+    '''  '''
+
+    pass
+
+
+async def delFunction(ctx, pKey: str, pAction: str, pValue: str, pData: dict):
+    '''  '''
+
+    pass
+
+
+@commands.has_permissions(administrator = True)
+@fenaverat.command(aliases = jsonLoad(pFile = f'{gDirectory}/setting.json')['aliases'])
+async def commandFunction(ctx):
+    '''  '''
+
+    await {
+
+        'set' : setFunction,
+        'get' : getFunction,
+        'delete' : deleteFunction
+
+    }[ctx.invoked_with.lower()](
+
+        ctx
+
+    )
 
 
 # main <
