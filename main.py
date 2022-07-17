374dfb368b96a84c6af2219e1d76e2e18295bebb
highlander <lxrbckl@gmail.com>
2022-07-16 20:44:09 -0500

    introduction of true backend of project fenaverat

--- main.py
+++ main.py
@@ -8,8 +8,8 @@
 
 
 # global <
-githubToken = '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'
-discordToken = 'OTk4MDI3ODExNjM1ODU5NDk3.GvDOHI.GvbzpImhcZ1x7K-NLyJmnDTO4XdIuhd4w4m3HY'
+githubToken = ''
+discordToken = ''
 
 # >
 
