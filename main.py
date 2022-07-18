bd2227cc65ccee913cd3c8f3f95a5cdc215e63f6
highlander <lxrbckl@gmail.com>
2022-07-18 00:17:49 -0500

    beta completion of fenaverat backend

--- main.py
+++ main.py
@@ -4,14 +4,20 @@
 # import <
 from os import path
 from github import Github
+from asyncio import sleep
 from discord import Intents
 from discord.ext import commands
-from lxRbckl import jsonLoad, jsonDump, githubSet
+from datetime import datetime as dt
+from lxRbckl import jsonLoad, jsonDump, githubSet, githubGet
 
 # >
 
 
 # global <
+gMinute = 180
+gFile = ''
+gBranch = ''
+gRepository = ''
 gPath = path.realpath(__file__).split('/')
 gDirectory = '/'.join(gPath[:(len(gPath) - 1)])
 githubToken = ''
@@ -24,42 +30,142 @@ discordToken = ''
 async def setFunction(ctx, pKey: str, pAction: str, pValue: str, pData: dict):
     '''  '''
 
-    pass
+    # if (new value) then set value <
+    if (pValue not in pData[pKey][pAction]):
+
+        await ctx.message.delete()
+        pData[pKey][pAction].append(pValue)
+        jsonDump(
+
+            pData = pData,
+            pFile = f'{gDirectory}/setting.json'
+
+        )
+
+    # >
 
 
-async def getFunction(ctx, pKey: str, pAction: str, pValue: str, pData: dict):
+async def getFunction(ctx, pKey: str, pValue: str, pAction: str, pData: dict):
     '''  '''
 
-    pass
+    await ctx.message.delete()
+    await ctx.channel.send(
 
-
-async def delFunction(ctx, pKey: str, pAction: str, pValue: str, pData: dict):
-    '''  '''
-
-    pass
-
-
-@commands.has_permissions(administrator = True)
-@fenaverat.command(aliases = jsonLoad(pFile = f'{gDirectory}/setting.json')['aliases'])
-async def commandFunction(ctx):
-    '''  '''
-
-    await {
-
-        'set' : setFunction,
-        'get' : getFunction,
-        'delete' : deleteFunction
-
-    }[ctx.invoked_with.lower()](
-
-        ctx
+        delete_after = 120,
+        content = '\n'.join(f'`{i}`' for i in pData[pKey][pAction])
 
     )
 
 
-# main <
-if (__name__ == '__main__'):
+async def delFunction(ctx, pKey: str, pValue: str, pAction: str, pData: dict):
+    '''  '''
 
-    pass
+    # if (existing value) then get value <
+    if (pValue in pData[pKey][pAction]):
+
+        await ctx.message.delete()
+        pData[pKey][pAction].remove(pValue)
+        jsonDump(
+
+            pData = pData,
+            pFile = f'{gDirectory}/setting.json'
+
+        )
+
+    # >
+
+
+@fenaverat.event
+async def on_ready(pGithub = Github(githubToken)):
+    '''  '''
+
+    aData = {}
+    while (True):
+
+        data = jsonLoad(pFile = f'{gDirectory}/setting.json')
+        bData = {'topic' : data['topic']['add'], 'language' : data['language']['add']}
+        for r in [r for u in data['user']['add'] for r in pGithub.get_user(u).get_repos()]:
+
+            # if (permitted project) <
+            if (r not in data['project']['remove']):
+
+                bData[r.full_name] = {
+
+                    'description' : r.description,
+                    'link' : f'https://github.com/{r.full_name}',
+                    'topic' : [t for t in r.get_topics() if (t not in data['topic']['remove'])],
+                    'language' : [l for l in r.get_languages() if (l not in data['language']['remove'])],
+                    'update' : dt.strptime(str(r.pushed_at).split(' ')[0], '%Y-%m-%d').strftime('%B %d %Y'),
+                    'feed' : githubGet(
+
+                        pGithub = pGithub,
+                        pFile = 'about.json',
+                        pRepository = r.full_name
+
+                    )
+
+                }
+
+            # >
+
+        # if (update) <
+        if (aData != bData):
+
+            githubSet(
+
+                pData = bData,
+                pFile = gFile,
+                pGithub = pGithub,
+                pBranch = gBranch,
+                pRepository = gRepository,
+                pMessage = 'Fenaverat Automated Message'
+
+            )
+            aData = bData
+
+        # >
+
+        await sleep(60 * gMinute)
+
+
+@commands.has_permissions(administrator = True)
+@fenaverat.command(aliases = jsonLoad(pFile = f'{gDirectory}/setting.json')['aliases'])
+async def commandFunction(ctx, pKey: str, pAction: str, pValue: str = None):
+    '''  '''
+
+    # local <
+    isAction = True if (pAction in ['add', 'remove']) else False
+    data = jsonLoad(pFile = f'{gDirectory}/setting.json')
+    isKey = True if (pKey in data.keys()) else False
+
+    # >
+
+    # if ((existing key) and (existing action)) <
+    if (isKey and isAction):
+
+        # call function <
+        await {
+
+            'set' : setFunction,
+            'get' : getFunction,
+            'del' : delFunction
+
+        }[ctx.invoked_with.lower()](
+
+            ctx = ctx,
+            pKey = pKey,
+            pValue = pValue,
+            pAction = pAction,
+            pData = jsonLoad(pFile = f'{gDirectory}/setting.json')
+
+        )
+
+        # >
+
+    # >
+
+
+# main <
+if (__name__ == '__main__'): fenaverat.run(discordToken)
 
 # >
